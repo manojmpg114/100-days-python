d20a30fd3e3d18a0199c22f6ea1f8abf808f7ca2
Manoj George <manojmpg114@aol.com>
2023-02-15 20:03:18 -0500

    Blackjack with additional notes complete

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -2,6 +2,7 @@
 import os
 from art import logo
 import random
+import sys
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
@@ -13,106 +14,109 @@ def deal_card(player_hand):
 def calculate_score(player_hand):
     ace_count = 0
     total_count = 0
-    for val in player_hand:
+    for val in player_hand: #Accounts for a differing 11 & 1 to represent Ace, otherwise could use total_count = sum(player_hand) 
         if val == 1 or val == 11:
             ace_count +=1
         else:
             total_count += val
     
+    #Alternative way to deal with ace counting  is check the list for any 11s and see the value if we do list.remove(11) and list.append(1)
+    #Depending on length of the check could be summed down to 2-3 lines of code making the switch
     if ace_count > 0 and total_count < 21:
         for i in range(ace_count):
             test = total_count + 11
-            if test < 21:
+            if test <= 21:
                 total_count = test
             else:
                 test = total_count + 1
-                if test < 21:
+                if test <= 21:
                     total_count = test
                 else:
                     return test
     
-    if ace_count > 0 and total_count > 21:
+    
+    
+  
+    
+    if ace_count > 0 and total_count >= 21:
         return total_count + ace_count #return ace count as a value of 1, BUST regardless
     
     return total_count
+
+#MAIN with overall logic
 if __name__ == '__main__':
-    play_game = input("Do you want to play ag ame of Blackjack? Type 'y' or 'n': ").lower()
     
-    user = []
-    dealer = []
+    play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
     
-    user_total = 0
-    dealer_total = 0
-    
-    ### card dealing and calculating works correctly, now need logic to determine how the game is played i.e checking player total scores and if dealing is allowed
-    
-    # for i in range(10):
-    #     user = deal_card(user)
+    if play_game != 'y':
+        sys.exit('Ok we can play another time')
         
-    #print(calculate_score([2,2,2,10,11,11,10]))
-    
-    print(user)
-    
-    
-
-
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
-
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+    keep_playing = True
+    while keep_playing:
+            
+        
+        print(logo)
+        
+        user = []
+        dealer = []
+        
+        user_total = 0
+        dealer_total = 0
+        
+        for i in range(0,2): #initial 2 cards, we can read our hand, we can't read the dealers and or we can read the dealers first card
+            user_total = calculate_score(deal_card(user))
+            dealer_total = calculate_score(deal_card(dealer))
+        
+        
+        keep_asking = True
+        while keep_asking and user_total <= 21:
+            
+            print(f'User Total: {user_total}')
+            hit = input("Do you want to hit or stay? type 'y' to hit or 'n' to stay: ")
+            if hit == 'y':
+                print('User Hits')
+                user_total = calculate_score(deal_card(user))
+            else:
+                keep_asking = False
+        
+        if user_total > 21:
+            user_total = calculate_score(deal_card(user))
+            print('BUST, YOU LOSE')
+        elif user_total == 21:
+            user_total = calculate_score(deal_card(user))
+            print('YOU WIN')
+        elif dealer_total < user_total:
+            #Dealers turn
+            print('Dealers turn:')
+        
+            while dealer_total < 17 or dealer_total == user_total or (dealer_total >= 17 and user_total > dealer_total):
+                
+                print(f'Dealer Total: {dealer_total}')
+                print('Dealer Hits')
+                dealer_total = calculate_score(deal_card(dealer))
+                if dealer_total > user_total and dealer_total <= 21:
+                    print(f'Dealer Total: {dealer_total}')
+                    print('HOUSE WINS')
+                elif dealer_total == user_total:
+                    print('DRAW')
+                    break
+                
+                if dealer_total > 21:
+                    print(f'Dealer Total: {dealer_total}')
+                    print('You Win, Dealer BUST')
+            
+            if dealer_total == 17 and dealer_total > user_total:
+                print(f'Dealer Total: {dealer_total}')
+                print('HOUSE WINS')
+        else:
+            print(f'User Total: {user_total}')
+            print(f'Dealer Total: {dealer_total}')
+            print('HOUSE WINS')
+                    
+            
+        again = input("Do you want to play another game? 'y' or 'n': ")
+        if again == 'y':
+            keep_playing = True
+            os.system('cls' if os.name == 'nt' else 'clear')
+        else:
+            keep_playing = False
