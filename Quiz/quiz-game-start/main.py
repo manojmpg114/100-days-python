e411f35ff426dc01760d103fa9563f201ff17f64
manojmpg114 <manojmpg114@aol.com>
2024-08-31 14:26:07 -0400

    quiz data change using list of dict from opentdb.com

--- Quiz/quiz-game-start/main.py
+++ Quiz/quiz-game-start/main.py
@@ -5,7 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for data in question_data:
-    question_bank.append(Question(data["text"],data["answer"]))
+    # question_bank.append(Question(data["text"],data["answer"]))
+    question_bank.append(Question(data["question"], data["correct_answer"]))
 
 # print(question_bank)
 
