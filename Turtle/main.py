d99239ba67a789fd3f73c7ceea6da6a58db69357
manojmpg114 <manojmpg114@aol.com>
2024-08-31 14:52:30 -0400

    Starting Turtle GUI work

---
+++ Turtle/main.py
@@ -0,0 +1,20 @@
+#Start of working with the Turtle Module
+
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+
+
+
+
+
+
+
+
+
+
+
+#screen portions of code have to stay on the bottom so we can do our turtle operations and then view the result / close on click
+screen = Screen()
+screen.exitonclick()
