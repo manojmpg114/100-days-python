b5b153b1876f91d46b1fad3bde4fc33c10e85614
manojmpg114 <manojmpg114@aol.com>
2024-09-01 20:15:21 -0400

    timmy the turtle

--- Turtle/main.py
+++ Turtle/main.py
@@ -1,10 +1,17 @@
-#Start of working with the Turtle Module
+#Start of working with the Turtle Module which is built upon Tkinter
 
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
+
+def draw_square():
+    for i in range(4):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(90)
+
+draw_square()
 
 
 
