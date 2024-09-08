4d7d7f056db5c8ce7b4b993b4d135d9690c8c383
Manoj George <manojmpg114@aol.com>
2024-09-08 19:30:30 -0400

    snake init

---
+++ Snake/snake_game/main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600,height=600) #-300 to 300 length and width to make 600x600
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+
+
+
+
+screen.exitonclick()
