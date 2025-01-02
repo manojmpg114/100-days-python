4260cbfe2f11f58ca97275b97a7f45ae503514c9
Manoj George <manojmpg114@aol.com>
2025-01-02 09:57:52 -0500

    Pong complete

--- Pong/pong_game/main.py
+++ Pong/pong_game/main.py
@@ -2,6 +2,7 @@ from turtle import Screen, Turtle
 import time
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800,height=600) #height -300,300 ; width -400,400
@@ -14,6 +15,7 @@ r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen() #have to be ready to move user-paddle
@@ -25,7 +27,7 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     
     screen.update()
     ball.move()
@@ -39,6 +41,15 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
     
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    
+    #Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
     
     
 screen.exitonclick()
