1d329e4b206f340fa0bb4e27bc7cebde6b89521e
Manoj George <manojmpg114@aol.com>
2025-01-13 15:43:56 -0500

    Read & Write Input names to custom letters

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -6,3 +6,21 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Names/invited_names.txt") as letters:
+    names = [name.rstrip() for name in letters] #strip \n in each name
+    # print(names)
+    with open("./Input/Letters/starting_letter.txt") as letter_template:
+        letter_temp = letter_template.read()
+        search_text = "[name]"
+        for name in names:
+            print(name)    
+            
+            custom_letter = letter_temp.replace(search_text, name)
+            
+            print(custom_letter)
+            
+            with open(f"./Output/ReadyToSend/{name}_letter.txt", mode="w") as letter_to_mail:
+                letter_to_mail.write(custom_letter)                       
+        
+        # print(letter_temp)
