938f4468561837a054925fec671253a9b0cdbc84
Manoj George <manojmpg114@aol.com>
2025-01-13 15:52:35 -0500

    Constant for placeholder in letter

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -7,16 +7,18 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+PLACE_HOLDER = "[name]"
+
 with open("./Input/Names/invited_names.txt") as letters:
     names = [name.rstrip() for name in letters] #strip \n in each name
     # print(names)
     with open("./Input/Letters/starting_letter.txt") as letter_template:
         letter_temp = letter_template.read()
-        search_text = "[name]"
+        # search_text = "[name]"
         for name in names:
             print(name)    
             
-            custom_letter = letter_temp.replace(search_text, name)
+            custom_letter = letter_temp.replace(PLACE_HOLDER, name)
             
             print(custom_letter)
             
