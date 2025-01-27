ee42b687e59b104d5846ac3fbade82e60cd05e4c
Manoj George <manojmpg114@aol.com>
2025-01-27 15:16:46 -0500

    tkinter widgets

--- scratch.py
+++ scratch.py
@@ -15,12 +15,82 @@ my_label["text"] = "New Text"
 
 def button_clicked():
     # print("I got clicked")
-    my_label.config(text= "Button got clicked")
+    new_text = input.get() #get the text inside the input box / textbox 
+    my_label.config(text= new_text)
 
 button = Button(text="CLICK ME", command=button_clicked) #use the command keyword arg and pass a function/method name to call that method on click
 button.pack()
 
 #Entry component
+
+input = Entry(width=30) #input field / text box for input on the gui
+input.insert(END,string="Some text to begin with.")
+input.pack()
+# print(input.get()) #we need to get this to work but it needs a trigger so maybe in the same button click
+
+#Multi-Line Text Box
+text = Text(height=5, width=30)
+#Put the cursor in textbox
+text.focus()
+#Add some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+print(text.get("1.0", END))
+text.pack()
+
+#Spinbox
+def spinbox_used():
+    #gets current value in spinbox.
+    print(spinbox.get())
+
+spinbox = Spinbox(from_=0, to= 10, width=5, command=spinbox_used)
+spinbox.pack()
+
+#Scale
+#called with current scale value
+def scale_used(value):
+    print(value)
+
+scale = Scale(from_=0, to = 100, command= scale_used)
+scale.pack()
+
+#Checkbutton
+def checkbutton_used():
+    #Prints 1 if on button checked, otherwise prints 0.
+    print(checked_state.get())
+    
+#variable to hold on to checked state, 0 is off, 1 is on
+checked_state = IntVar() #IntVar is a class within the tkinter module that can hold the variable state of some tkinter widgets / components
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+#Radiobutton
+def radio_used():
+    print(radio_state.get())
+    
+#Variable to hold on to which radio button value is checked 
+radio_state = IntVar() #IntVar is a class within the tkinter module that can hold the variable state of some tkinter widgets / components
+radiobutton1 = Radiobutton(text="Option1", value = 1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value = 2, variable=radio_state, command=radio_used)
+
+radiobutton1.pack()
+radiobutton2.pack()
+
+#Listbox
+def listbox_used(event):
+    #Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+
+# print(type(listbox))
+
+listbox.bind("<<ListboxSelect>>",listbox_used)
+listbox.pack()
 
 window.mainloop()
 
