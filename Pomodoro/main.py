a957ca8348d103358bc5570432ee9189987e7e75
Manoj George <manojmpg114@aol.com>
2025-01-30 16:27:00 -0500

    Canvas widget, PhotoImage class tkinter module

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -13,7 +13,7 @@ Work Timer: 25min
 Break Timer: 20min
 
 """
-
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -32,3 +32,18 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100,pady=50, bg=YELLOW, highlightthickness=0)
+
+
+#Canvas Widget - Allows for layering / drawing one on top of another 
+canvas = Canvas(width=200, height=224, bg=YELLOW, )
+tomato_img = PhotoImage(file="tomato.png") #specific class from the tkinter module that helps pass images, requires the file = file or file path location if not relative dir
+canvas.create_image(102, 112, image=tomato_img)
+canvas.create_text(102, 130, text="00:00", fill="white", font=((FONT_NAME, 35, "bold")))
+canvas.pack()
+
+
+window.mainloop()
