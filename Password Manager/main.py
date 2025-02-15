185253c4c00b4b76ce2697534f09577f44e45b54
Manoj George <manojmpg114@aol.com>
2025-02-15 16:21:20 -0500

    exception handling and JSON data storage

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import * #imports all the classes constants
 from tkinter import messagebox #have to import separately because its a different module of code
 from random import choice, randint, shuffle
+import json
 #could pip install pyperclip and then import pyperclip to allow us to copy and paste a generated password but we will avoid that here
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -64,17 +65,31 @@ def save():
     website = website_entry.get()
     email = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {"email": email,
+                  "password": password}}
     
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
-    else:    
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \n Is it ok to save?")
-        
-        
-        if is_ok:    
+    else:      
+        try:
+           with open("data.json", "r") as data_file:
+               #Reading old data
+               data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent = 4)
+        except json.JSONDecodeError:  # File exists but is empty or not valid JSON
+            data = new_data
+        else:
+            #Updating old data with new data
+            data.update(new_data)
             
-            with open("data.txt", 'a') as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            with open("data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+                
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
